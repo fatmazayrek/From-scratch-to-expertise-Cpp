a2b7396d6f1a101180ade99488afe8fc84a12eb7
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2022-10-20 23:38:16 +0300

    Update Structure with Constructor.cpp

--- Structure with Constructor.cpp
+++ Structure with Constructor.cpp
@@ -22,3 +22,11 @@ int main(){
     Student s ; 
     cout << s.getName();
 }
+
+/*According to this example, the following question may have arisen in your mind: "So what is the difference between structure and class?"
+I would like to briefly mention the differences:
+- all structures are in "public" state when the structure is first created. However, when the class is created, all its structures are created as "private".
+- class inherits while structure does not inherit
+- Class can be accessed by references, while structures can be accessed directly.
+data for detailed information: 
+"https://bidb.itu.edu.tr/seyir-defteri/blog/2013/09/06/yap%C4%B1-(struct)-kavram%C4%B1-ve-yap% Differences between C4%B1-and-s%C4%B1n%C4%B1f-(class)-between%C4%B1" */
