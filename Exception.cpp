6ac75296750aaad417633e34e6f2b8d449e0d5a0
fatmazayrek <fzayrek@gmail.com>
2022-11-15 22:49:39 +0300

    Create Exception.cpp

---
+++ Exception.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Let's make a simple example about "Exception"
+
+int main(){
+
+    int age;
+
+    while(true){
+
+        cout << "Enter your age: ";
+        cin >> age;
+
+        try{
+            if(age < 0){
+
+                throw 0;
+            }
+        }
+        catch (int error) {
+
+            cout << "Age can not be less than 0" << endl;
+        }
+    }
+}
+
