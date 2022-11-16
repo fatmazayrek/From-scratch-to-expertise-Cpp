da1f145daafd3d4ea95846d6b84f4fef1aa3022b
fatmazayrek <fzayrek@gmail.com>
2022-11-16 18:20:48 +0300

    Create Writing Array of Objects.cpp

---
+++ Writing Array of Objects.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+//#define N 5
+
+using namespace std;
+
+class Person {
+private:
+    string name, surname;
+
+public:
+    // dummy constructor that you should write
+    Person() {}
+
+    Person(string name, string surname): name(name), surname(surname){}
+
+// You can also write this way:
+//    Person(int x, int y)
+//    {
+//        this->x = x;
+//        this->y = y;
+//    }
+
+    // function to print
+    void print() { cout << name << " " << surname << endl; }
+};
+
+int main()
+{
+    cout << "How many people will you write: ";
+    int N;
+    cin >> N;
+    Person* per = new Person[N];
+    string name, surname;
+
+    // calling constructor
+    // for each index of array
+    for (int i = 0; i < N; i++) {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter surname: ";
+        cin >> surname;
+        per[i] = Person(name, surname);
+    }
+
+    // printing contents of array
+    for (int i = 0; i < N; i++) {
+        per[i].print();
+    }
+}
