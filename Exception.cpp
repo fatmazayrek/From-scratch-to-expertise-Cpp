99ab92f104d00eccf2385522c09d8a4948f1f7fb
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:35:47 +0300

    Update Exception.cpp

--- Exception.cpp
+++ Exception.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 // Let's make a simple example about "Exception"
 
-int main(){
-
+int main()
+{
     int age;
 
-    while(true){
-
+    while(true)
+    {
         cout << "Enter your age: ";
         cin >> age;
 
-        try{
-            if(age < 0){
-
+        try
+        {
+            if(age < 0)
+            {
                 throw 0;
             }
         }
-        catch (int error) {
-
+        catch (int error)
+        {
             cout << "Age can not be less than 0" << endl;
         }
     }
