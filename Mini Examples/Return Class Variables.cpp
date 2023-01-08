8ad94debef5338cfd7ebb6d39ab25b85e7bd2a59
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:15:17 +0300

    Update Return Class Variables.cpp

--- Mini Examples/Return Class Variables.cpp
+++ Mini Examples/Return Class Variables.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 // Let's make a simple example about "return"
 
-class Person{
+class Person
+{
 public:
     int age;
     string name;
@@ -12,7 +13,8 @@ public:
     Person(){}
 };
 
-Person returnFunc(string name, int age){
+Person returnFunc(string name, int age)
+{
     Person p;
     p.name = name;
     p.age = age;
