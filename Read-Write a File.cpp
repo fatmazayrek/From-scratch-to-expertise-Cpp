87b695bb5d6e80fbe7ec917579bcf02678261c95
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:38:13 +0300

    Update Read-Write a File.cpp

--- Read-Write a File.cpp
+++ Read-Write a File.cpp
@@ -21,25 +21,29 @@ using namespace std;
                 Lecture: Math, Business
     3) Read this information from the file named Informations of Students. */
 
-int main(){
-
+int main()
+{
     // First Example ------>
     ofstream file("C:\\Users\\Fatma\\OneDrive\\Information_of_Students.txt");
     //add new information into the end of the file
 
-    try {                           //confirm that the file is exist
+    try 
+    {                           //confirm that the file is exist
         if(!((file << "Student 1 : " << endl << " - Name: Fatma Zayrek" << endl << " - Number: 45" << endl << " - Lecture: Math" << endl) &&
         (file << "Student 2 : " << endl << " - Name: Micheal Scott" << endl << " - Number: 489" << endl << " - Lecture: Business" << endl) &&
         (file << "Student 3 : " << endl << " - Name: Jim Halpert" << endl << " - Number: 128" << endl << " - Lecture: Math, Business" << endl)))
                 throw 0;
-    } catch (int a) {
-        cout << "There is not exist this file";
     }
+   catch (int a)
+   {
+       cout << "There is not exist this file";
+   }
 
     ifstream file2("C:\\Users\\Fatma\\OneDrive\\Information_of_Students.txt");
     string name;
 
-    while(file2 >> name){
+    while(file2 >> name)
+    {
         if(name == "-")
             cout << endl;
         cout << name;
