56e4fa7bc839cf7c575c9c0b844b5f2359d5b69c
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:36:12 +0300

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 // In this example, I want to show you how you can use "list" ----->
 
-class People{
+class People
+{
 private:
     string name;
     int age;
@@ -13,13 +14,15 @@ private:
 public:
     People(){}
 
-    void setInfo(string name, int age, bool marrial_status){
+    void setInfo(string name, int age, bool marrial_status)
+    {
         this->name = name;
         this->age = age;
         this->marrial_status = marrial_status;
     }
 
-    void getInfo(){
+    void getInfo()
+    {
         cout << "Name: " << this->name << endl << "Age: " << this->age << endl << "Marrial Status: " << this->marrial_status << endl;
     }
 };
@@ -45,7 +48,8 @@ int main()
     people.push_back(p4);
     people.push_front(p5);
 
-    for(list<People>::iterator iterat = people.begin(); iterat != people.end() ; iterat++){
+    for(list<People>::iterator iterat = people.begin(); iterat != people.end() ; iterat++)
+    {
         iterat->getInfo();
     }
 }
