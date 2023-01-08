95d342bea2da06346d97d16b5f34ba236c1072cb
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 16:25:20 +0300

    Update Pointer to Pointer.cpp

--- Mini Examples/Pointers/Pointer to Pointer.cpp
+++ Mini Examples/Pointers/Pointer to Pointer.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int x = 10;
     int y = 20;
@@ -46,42 +47,42 @@ Reference: https://necatiergin2019.medium.com/pointer-to-pointer-g%C3%B6sterici-
 
 /* Output will be: 
 	x: 10
-	x address: 0x61fe14
+	x address: 0x66fe14
 	y: 20
-	y address: 0x61fe10
-	ptr adress: 0x61fe14
+	y address: 0x66fe10
+	ptr adress: 0x66fe14
 	ptr value: 10
-	pp: 0x61fe08
-	*pp: 0x61fe14
+	pp: 0x66fe08
+	*pp: 0x66fe14
 	**pp: 10
 	2 ---------------------------------------------------
 	x: 100
-	x address: 0x61fe14
-	y: 200
-	y address: 0x61fe10
-	ptr adress: 0x61fe10
-	ptr value: 200
-	pp: 0x61fe08
-	*pp: 0x61fe10
-	**pp: 200
+	x address: 0x66fe14
+	y: 20
+	y address: 0x66fe10
+	ptr adress: 0x66fe10
+	ptr value: 20
+	pp: 0x66fe08
+	*pp: 0x66fe10
+	**pp: 20
 	3 ---------------------------------------------------
 	x: 100
-	x address: 0x61fe14
+	x address: 0x66fe14
 	y: 200
-	y address: 0x61fe10
-	ptr adress: 0x61fe10
+	y address: 0x66fe10
+	ptr adress: 0x66fe10
 	ptr value: 200
-	pp: 0x61fe08
-	*pp: 0x61fe10
+	pp: 0x66fe08
+	*pp: 0x66fe10
 	**pp: 200
 	4 ----------------------------------------------------
 	x: 100
-	x address: 0x61fe14
+	x address: 0x66fe14
 	y: 2000
-	y address: 0x61fe10
-	ptr adress: 0x61fe10
+	y address: 0x66fe10
+	ptr adress: 0x66fe10
 	ptr value: 2000
-	pp: 0x61fe08
-	*pp: 0x61fe10
+	pp: 0x66fe08
+	*pp: 0x66fe10
 	**pp: 2000
 */
