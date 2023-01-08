7931eb9a989541e79603e3987fb3a3dab2e302f7
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:38:49 +0300

    Update Vectors.cpp

--- Vectors.cpp
+++ Vectors.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 // In this example, I want to show you how you can use "vector" ----->
 
-class People{
+class People
+{
 private:
     string name;
     int age;
@@ -13,13 +14,15 @@ private:
 public:
     People(){}
 
-    void setInfo(string name, int age, bool marrial_status){
+    void setInfo(string name, int age, bool marrial_status)
+    {
         this->name = name;
         this->age = age;
         this->marrial_status = marrial_status;
     }
 
-    void getInfo(){
+    void getInfo()
+    {
         cout << "Name: " << this->name << endl << "Age: " << this->age << endl << "Marrial Status: " << this->marrial_status << endl;
     }
 };
@@ -34,8 +37,8 @@ int main()
     bool marrial;
     cout << "Enter your people's information: " << endl;
 
-    while(a<5){
-
+    while(a < 5)
+    {
         cout << "Name: " ;
         cin >> name;
         cout << "Age: " ;
@@ -47,8 +50,8 @@ int main()
         a++;
     }
 
-    for(vector<People>::iterator iterat = people.begin() ; iterat != people.end() ; iterat++){
+    for(vector<People>::iterator iterat = people.begin() ; iterat != people.end() ; iterat++)
+    {
         iterat->getInfo();
     }
-
 }
