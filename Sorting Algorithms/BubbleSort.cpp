530f8562a2a6855995eb73c9ff318a4c2be6fafb
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:32:47 +0300

    Update BubbleSort.cpp

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -9,13 +9,17 @@ using namespace std;
 int *bubbleSort(int arr[], int n)
 {
     int i, j, temp;
-    for (i = 0; i < n - 1; i++){
+    for (i = 0; i < n - 1; i++)
+    {
         for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1]){
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j + 1] = temp;
             }
+        }
     }
 
     return arr;
@@ -28,7 +32,8 @@ int main()
 
     int *arr2 = bubbleSort(arr, N);
 
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N; i++)
+    {
         cout << arr2[i] << " ";
     }
 }
