3e76b385458d7b803388616f84f68a8236d285d4
fatma zayrek <91613858+fatmazayrek@users.noreply.github.com>
2023-01-08 17:31:46 +0300

    Update BinarySearch.cpp

--- Searching Algorithms/BinarySearch.cpp
+++ Searching Algorithms/BinarySearch.cpp
@@ -7,20 +7,25 @@ template <typename T>
 
 //Binary Search Algorithm --->
 
-int binarySearch(T *arr, T element, int size){
+int binarySearch(T *arr, T element, int size)
+{
 
     int h = size-1 , l=0, mid;
 
-    while(h - l >= 0){
+    while(h - l >= 0)
+    {
         mid = l + (h - l) / 2;
 
-        if(arr[mid] == element){
+        if(arr[mid] == element)
+        {
             return mid;
         }
-        else if(arr[mid] < element){
+        else if(arr[mid] < element)
+        {
             l = mid + 1;
         }
-        else{
+        else
+        {
             h = mid - 1;
         }
     }
@@ -33,21 +38,21 @@ int* selectionSort(int arr[], int n)
     int i, j, temp, min_index;
 
     for (i = 0; i < n-1; i++)
+    {
+        min_index = i;
+
+        for (j = i+1; j < n; j++)
         {
-            min_index = i;
-
-            for (j = i+1; j < n; j++){
-
-                if (arr[j] < arr[min_index]){
-                    min_index = j;
-                }
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
             }
-
-            temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
         }
 
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
     return arr;
 }
 
