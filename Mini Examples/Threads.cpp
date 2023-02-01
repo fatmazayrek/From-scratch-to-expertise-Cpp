9577477c2c625aba9de4b401394ac49aff3ad3ac
fatma zayrek <fzayrek@gmail.com>
2023-02-01 16:21:43 +0300

    Create Threads.cpp

---
+++ Mini Examples/Threads.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void Thread1(int a, int b)
+{
+    for(int i=0; i<3; i++)
+    {
+        std::cout << "Thread1: " << a * b << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    }
+
+    std::cout << "Tread1 is ending... " << std::endl;
+}
+
+void Thread2(std::string name)
+{
+    for(int i=0; i<3; i++)
+    {
+        std::cout << "Thread2: " << name << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+        std::cout << "Tread2 is ending... " << std::endl;
+
+}
+
+int main()
+{
+    std::thread t1(Thread1, 12, 45);
+    std::thread t2(Thread2, "Fatma");
+
+    t1.detach();
+    t2.detach();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    //std::thread::join() waits for other thread finish
+//    t1.join();
+//     t2.join(); 
+}
+
+/* Output will be: 
+        Thread1: 540
+        Thread2: Fatma
+        Thread1: 540
+        Thread2: Fatma
+        Thread1: 540
+        Tread1 is ending...
+        Thread2: Fatma
+        Tread2 is ending... 
+*/
